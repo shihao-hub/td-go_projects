1e59b7ee97aedeed1a763b07e5b8aff7e2991d34
shihao-hub <2958017271@qq.com>
2026-09-12 10:46:49 +0800

    aiquick:chore: 归档移除至父仓库 .archived

--- aiquick/internal/capture/capture_other.go
+++
@@ -1,6 +0,0 @@
-//go:build !windows
-
-package capture
-
-// SelectedText 非 Windows 平台不支持划词。
-func SelectedText(maxLen int) (string, bool) { return "", false }
--- aiquick/internal/client/proc_other.go
+++
@@ -1,7 +0,0 @@
-//go:build !windows
-
-package client
-
-import "syscall"
-
-func procAttr() *syscall.SysProcAttr { return nil }
